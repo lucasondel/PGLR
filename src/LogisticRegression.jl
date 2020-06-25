501166f19fb5cdb6fb3aabfc907a2dcabc134b04
Lucas Ondel <iondel@fit.vutbr.cz>
2020-06-25 10:34:05 +0200

    Temporary commit with iso normal binary LR

--- src/LogisticRegression.jl
+++ src/LogisticRegression.jl
@@ -4,16 +4,36 @@
 using LinearAlgebra
 
 export BinaryLogisticRegression
+export BinaryLogisticRegressionIso
 export LogisticRegression
 export predict
+export regressors
+
+#######################################################################
+# Helper functions
+#
+
+function regressors(hasbias::Bool, X::Matrix{T}) where T <: AbstractFloat
+    if hasbias
+        # We add an extra dimension always set to "1" to incorporate a bias
+        # parameter to the model.
+        return vcat(X, ones(T, 1, size(X, 2)))
+    end
+    return X
+end
+
+# Return the Polya-Gamma posterior of the augmented
+# logistic function
+function _augment_posterior(ψ²::Vector{T}) where T <: AbstractFloat
+    b = ones(T, length(ψ²))
+    qω = PolyaGamma(b)
+    update!(qω, -.5 * ψ²)
+    return qω
+end
 
 #######################################################################
 # Binary logistic regression
 #
-# NOTE:
-# We discourage the use of BinaryLogisticRegression model directly.
-# Instead, you should use the general LogisticRegression model (which
-# uses the BinaryLogisticRegression itself).
 
 struct BinaryLogisticRegression <: Model
     β::ConjugateParameter{<:Normal}
@@ -34,7 +54,7 @@ function BinaryLogisticRegression(μ₀::Vector{T}, Σ₀::Matrix{T},
                              hasbias)
 end
 
-function BinaryLogisticRegression(T; inputdim::Integer, initσ::Real = 0.0,
+function BinaryLogisticRegression(T = Float64; inputdim::Integer, initσ::Real = 0.0,
                                   pseudocounts::Real = 1.0, hasbias::Bool = true)
     dim = hasbias ? inputdim + 1 : inputdim
 
@@ -49,38 +69,13 @@ function BinaryLogisticRegression(T; inputdim::Integer, initσ::Real = 0.0,
     BinaryLogisticRegression(μ₀, Σ₀, μ, Σ, hasbias)
 end
 
-function BinaryLogisticRegression(;inputdim::Integer, initσ::Real = 0.0,
-                                  pseudocounts::Real = 1.0, hasbias::Bool = true)
-    BinaryLogisticRegression(Float64, inputdim = inputdim, initσ = initσ,
-                             pseudocounts = pseudocounts, hasbias = hasbias)
-end
-
-function regressors(model::BinaryLogisticRegression, X::Matrix{T}) where T <: AbstractFloat
-    if model.hasbias
-        # We add an extra dimension always set to "1" to incorporate a bias
-        # parameter to the model.
-        return vcat(X, ones(T, 1, size(X, 2)))
-    end
-    return X
-end
-
-
-# Return the Polya-Gamma posterior of the augmented
-# logistic function
-function _augment_posterior(ψ²::Vector{T}) where T <: AbstractFloat
-    b = ones(T, length(ψ²))
-    qω = PolyaGamma(b)
-    update!(qω, -.5 * ψ²)
-    return qω
-end
-
 # Return the sufficient statistics of the log likelihood and the
 # the expected statistics of β.
 function _precompute(model::BinaryLogisticRegression, X::Matrix{T1},
                      z::Vector{T2},
                      Nₖ::Union{Vector{T2}, Nothing} = nothing) where {T1 <: AbstractFloat,
                                                                       T2 <: Real}
-    X̂ = regressors(model, X)
+    X̂ = regressors(model.hasbias, X)
 
     # Quadratic expansion of the regressors
     D, N = size(X̂)
@@ -136,9 +131,113 @@ function predict(model::BinaryLogisticRegression,
     # In Bishop 4.5.2 a very similar approximation uses a = π / 8 ≈ 0.392...
 
     μ, Σ = stdparam(model.β.posterior)
-    X̂ = regressors(model, X)
+    X̂ = regressors(model.hasbias, X)
 
     σ²s = [sum((Σ .* x̂') .* x̂) for x̂ in eachcol(X̂)]
+    ψs = (X̂' * μ) ./ sqrt.(1 .+ a * σ²s)
+    1 ./ (1 .+ exp.(-ψs))
+end
+
+#######################################################################
+# Binary logistic regression with isotropic cov. posterior
+#
+
+struct BinaryLogisticRegressionIso <: Model
+    β::ConjugateParameter{<:NormalIso}
+    hasbias::Bool
+
+    function BinaryLogisticRegressionIso(β::ConjugateParameter{<:NormalIso}, hasbias::Bool)
+        model = new(β, hasbias)
+        # The model is not fully conjugate so no easy stats accumulation.
+        #β.stats = data -> stats_β(model, data...)
+        return model
+    end
+end
+
+function BinaryLogisticRegressionIso(μ₀::Vector{T}, v₀::Real,
+                                     μ::Vector{T}, v::Real,
+                                     hasbias::Bool) where T <: AbstractFloat
+    param = ConjugateParameter(NormalIso(μ₀, v₀), NormalIso(μ, v))
+    BinaryLogisticRegressionIso(param, hasbias)
+end
+
+function BinaryLogisticRegressionIso(T = Float64; inputdim::Integer, initσ::Real = 0.0,
+                                     pseudocounts::Real = 1.0, hasbias::Bool = true)
+    dim = hasbias ? inputdim + 1 : inputdim
+
+    # Prior parameters
+    μ₀ = zeros(T, dim)
+    v₀ = T(1/pseudocounts)
+
+    # Initial paremeterization of the posterior
+    μ = μ₀ .+ T(initσ) .* randn(T)
+    v = T(1/pseudocounts)
+
+    BinaryLogisticRegressionIso(μ₀, v₀, μ, v, hasbias)
+end
+
+# Return the sufficient statistics of the log likelihood and the
+# the expected statistics of β.
+function _precompute(model::BinaryLogisticRegressionIso, X::Matrix{T1},
+                     z::Vector{T2},
+                     Nₖ::Union{Vector{T2}, Nothing} = nothing) where {T1 <: AbstractFloat,
+                                                                      T2 <: Real}
+    X̂ = regressors(model.hasbias, X)
+    D, N = size(X̂)
+
+    # Mean and (isotropic) variance of the variational posterior
+    m, v = stdparam(model.β.posterior)
+
+    # "Isotropic" quadratic expansion of the regressors
+    X̂ᵀX̂ = sum(X̂ .* X̂, dims = 1) / D
+    E_ψ = m' * X̂
+    E_ψ² = dropdims(v * X̂ᵀX̂ .+ E_ψ .^ 2, dims = 1)
+
+    # Augmentation of the logistic function
+    pω = PolyaGamma{T1, size(X̂, 2)}()
+    qω = _augment_posterior(E_ψ²)
+
+    m, v, dropdims(E_ψ, dims = 1), E_ψ², mean(qω), pω, qω
+end
+
+function (model::BinaryLogisticRegressionIso)(
+    X::Matrix{T1},
+    z::Vector{T2},
+    Nₖ::Union{Vector{T2}, Nothing} = nothing
+) where {T1 <: AbstractFloat, T2 <: Real}
+
+    m, v, E_ψ, E_ψ², E_ω, pω, qω = _precompute(model, X, z, Nₖ)
+
+    # KL divegerence betwen qω and pω for each sample.
+    q_η, p_η = naturalparam(qω), naturalparam(pω)
+    KL = lognorm(pω, perdim = true) - lognorm(qω, perdim = true) - (p_η .- q_η) .* gradlognorm(qω)
+
+
+    (z .- 0.5) .* E_ψ - 0.5 * E_ω .* E_ψ²   .- log(2) .- KL
+end
+
+function stats_β(
+    model::BinaryLogisticRegressionIso,
+    X::Matrix{T1},
+    z::Vector{T2},
+    Nₖ::Union{Vector{T2}, Nothing} = nothing
+) where {T1 <: AbstractFloat, T2 <: Real}
+
+    _precompute(model, X, z, Nₖ)[1]
+end
+
+function predict(model::BinaryLogisticRegressionIso,
+                 X::Matrix{T}; a::T = 0.368) where T <: AbstractFloat
+    # Following https://arxiv.org/pdf/1703.00091.pdf, we use the
+    # approximation:
+    # ⟨σ(ψ)⟩ ≈ σ(μ / √(1 + a * σ²))
+    # where a = 0.368
+    # In Bishop 4.5.2 a very similar approximation uses a = π / 8 ≈ 0.392...
+
+    μ, v = stdparam(model.β.posterior)
+    X̂ = regressors(model.hasbias, X)
+
+    σ²s = [sum((v .* x̂') .* x̂) for x̂ in eachcol(X̂)]
     ψs = (X̂' * μ) ./ sqrt.(1 .+ a * σ²s)
     1 ./ (1 .+ exp.(-ψs))
 end
@@ -174,7 +273,7 @@ function LogisticRegression(μ₀::Vector{T}, Σ₀::Matrix{T}, μ::Vector{T},
                         for i in 1:nclasses-1])
 end
 
-function LogisticRegression(T; inputdim::Integer, nclasses::Integer,
+function LogisticRegression(T = Float64; inputdim::Integer, nclasses::Integer,
                             initσ::Real = 0.0, pseudocounts::Real = 1.0,
                             hasbias::Bool = true)
     dim = hasbias ? inputdim + 1 : inputdim
@@ -188,14 +287,6 @@ function LogisticRegression(T; inputdim::Integer, nclasses::Integer,
     Σ = deepcopy(Σ₀)
 
     LogisticRegression(μ₀, Σ₀, μ, Σ, hasbias, nclasses = nclasses)
-end
-
-function LogisticRegression(;inputdim::Integer, nclasses::Integer,
-                            initσ::Real = 0.0, pseudocounts::Real = 1.0,
-                            hasbias::Bool = true)
-    LogisticRegression(Float64, inputdim = inputdim, nclasses = nclasses,
-                       initσ = initσ, pseudocounts = pseudocounts,
-                       hasbias = hasbias)
 end
 
 getconjugateparams(model::LogisticRegression) = [m.β for m in model.stickbreaking]
@@ -220,6 +311,10 @@ function (model::LogisticRegression)(X::Matrix{T1}, z::Vector{T2},) where {T1 <:
     onehot, Nₖ = _encode(model, z)
     retval = zeros(T1, size(X, 2))
     for (k, m) in enumerate(model.stickbreaking)
+        s1 = onehot[k, z .>= k]
+        if length(s1) < 1
+            continue
+        end
         retval[z .>= k] .+= m(X[:, z .>= k], onehot[k, z .>= k], Nₖ[k, z .>= k])
     end
     retval
